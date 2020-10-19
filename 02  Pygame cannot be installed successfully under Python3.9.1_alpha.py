9db3bfba91dd163e56df2515168209834db0c9c0
MrZ <zhangziwen@sjtu.edu.cn>
2020-10-19 23:57:05 +0800

    Delete 02  Pygame cannot be installed successfully under Python3.9.1_alpha.py

--- 02  Pygame cannot be installed successfully under Python3.9.1_alpha.py
+++
@@ -1,8 +0,0 @@
-'''
-The 'pygame' cannot be installed successfully under Python3.9.1_alpha.
-I don't know whether it's the problem of compatibility or not.
-
-I have already upgrated my pip and setuptools, but the terminal still gives me an ERROR Message:
-    ERROR: Command errored out with exit status 1: python setup.py egg_info Check the logs for full command output.
-'''
-# 2020.02.25 After I got back to stable version Python3.8, it can be installed successfully.
